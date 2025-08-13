15668be96a5c972b626f3da3d4858acd088c548a
Sarun Daunghirun <sdsarun@outlook.com>
2025-08-14 03:22:50 +0700

    refactor: move folder from src to package and change peer version

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['package/index.ts'],
   treeshake: true,
   splitting: false,
   sourcemap: true,
